72c37f1aec8bc43b0983bfa2eb05c6f0be97a7d7
Rina Deka <rdeka@ncsu.edu>
2023-07-09 22:44:35 -0400

    Fixed blog post

--- RenderScript.R
+++ RenderScript.R
@@ -1,5 +1,5 @@
 rmarkdown::render("C:/Users/rsdek/Documents/repos/rinadeka.github.io/_Rmd/Project2.Rmd",
-                  output_file = "C:/Users/rsdek/Documents/repos/rinadeka.github.io/posts/2023-06-29-Coolest-Thing-I-Learned.md",
+                  output_file = "C:/Users/rsdek/Documents/repos/rinadeka.github.io/posts/Project2.md",
                   output_format=' github_document',
                   output_dir = "C:/Users/rsdek/Documents/repos/rinadeka.github.io/_posts",
                   output_options = list(html_preview = FALSE,keep_html=FALSE))
