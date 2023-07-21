89c92a277b26840922a75140c0055acf68a2a525
Rina Deka <rdeka@ncsu.edu>
2023-07-21 14:09:04 -0400

    Blog post; delay due to heat illness

---
+++ RenderScript.R
@@ -0,0 +1,5 @@
+rmarkdown::render("C:/Users/rsdek/Documents/repos/rinadeka.github.io/_Rmd/2023-07-20-MachineLearning.Rmd",
+                  output_file = "C:/Users/rsdek/Documents/repos/rinadeka.github.io/posts/2023-07-20-MachineLearning.md",
+                  output_format=' github_document',
+                  output_dir = "C:/Users/rsdek/Documents/repos/rinadeka.github.io/_posts",
+                  output_options = list(html_preview = FALSE,keep_html=FALSE))
